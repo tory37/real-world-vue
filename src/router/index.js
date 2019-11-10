6728c4cb2c628d507195d1c9354a8a91d8a3b41c
Tory <tory37@gmail.com>
2019-11-10 00:46:32 -0600

    Finished lesson 5

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,33 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import EventCreate from '@/views/EventCreate';
+import EventList from '@/views/EventList';
+import EventShow from '@/views/EventShow';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: Home
+    path: '/',
+    name: 'event-list',
+    component: EventList
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/event/:id',
+    name: 'event-show',
+    component: EventShow,
+    props: true
+  },
+  {
+    path: '/event/create',
+    name: 'event-create',
+    component: EventCreate
   }
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
 });
