f93590b44dbce74cde36b730fda63be03a5865e7
Tory <tory37@gmail.com>
2019-11-10 14:36:15 -0600

    Finished lesson 7

--- src/main.js
+++ src/main.js
@@ -1,12 +1,33 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
+// Global Component Imports
+import upperFirst from 'lodash/upperFirst';
+import camelCase from 'lodash/camelCase';
+
+const requireComponent = require.context(
+  './components', // the relative path of the directory to search
+  false, // subdirectories will not be searched
+  /Base[A-Z]\w+\.(vue|js)$/ // regular expression that searches for components starting with "Base" and ending in .vue or .js
+);
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName);
+
+  const componentName = upperFirst(
+    camelCase(
+      fileName.replace(/^\.\/(.*)\.\w+$/, '$1') // removes what's before and after the filename itself
+    )
+  );
+
+  Vue.component(componentName, componentConfig.default || componentConfig);
+});
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
